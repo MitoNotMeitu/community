90b4642ed0d15921e16b35f0efc96d855c283549
MitoNotMeitu <haruhi20030225@gmail.com>
2024-02-04 16:47:42 +0800

    add index and bootstrap

--- src/main/java/life/majiang/community/controller/IndexController.java
+++ src/main/java/life/majiang/community/controller/IndexController.java
@@ -6,10 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller//controller:我允许这个类去接收前端的一个请求
-public class HelloController {
-    @GetMapping("/hello")
-    public String hello(@RequestParam(name="name") String name,Model model){
-        model.addAttribute("name",name);
-        return "hello";//执行到此就会自动去找hello的模版，位于resources/templates中
-    }
+public class IndexController {
+    @GetMapping("/")
+    public String index() { return "index"; }
 }
