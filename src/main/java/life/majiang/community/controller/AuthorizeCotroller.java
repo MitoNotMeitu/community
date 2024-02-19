9f17cfa0117a3c4856ee9887e404314b8b8a29c8
MitoNotMeitu <haruhi20030225@gmail.com>
2024-02-19 23:49:20 +0800

    add check cookies

--- src/main/java/life/majiang/community/controller/AuthorizeCotroller.java
+++ src/main/java/life/majiang/community/controller/AuthorizeCotroller.java
@@ -11,7 +11,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.util.UUID;
 
 @Controller
@@ -35,7 +37,7 @@ public class AuthorizeCotroller {
     @GetMapping("/callback")
     public String callback(@RequestParam(name="code") String code,
                            @RequestParam(name="state") String state,
-                           HttpServletRequest request){//使用HttpServletRequest后，Springboot会自动将上下文中的request传进来。而session就是在request当中的。
+                           HttpServletResponse response){//使用HttpServletRequest后，Springboot会自动将上下文中的request传进来。而session就是在request当中的。
         AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
         accessTokenDTO.setClient_id(clientId);
         accessTokenDTO.setClient_secret(clientSecret);
@@ -46,14 +48,14 @@ public class AuthorizeCotroller {
         GithubUser githubUser = githubProvider.getUser(accessToken);
         if (githubUser !=null){
             User user = new User();
-            user.setToken(UUID.randomUUID().toString());
+            String token = UUID.randomUUID().toString();
+            user.setToken(token);
             user.setName(githubUser.getName());
             user.setAccountId(String.valueOf(githubUser.getId()));
             user.setGmtCreate(System.currentTimeMillis());
             user.setGmtModified(user.getGmtCreate());
             userMapper.insert(user);
-            //登陆成功，写入cookie和session
-            request.getSession().setAttribute("user", githubUser);
+            response.addCookie(new Cookie("token",token));
             return "redirect:/";
         }else {
             //不成功，重新登陆
--- src/main/java/life/majiang/community/mapper/UserMapper.java
+++ src/main/java/life/majiang/community/mapper/UserMapper.java
@@ -4,10 +4,15 @@ package life.majiang.community.mapper;
 import life.majiang.community.model.User;
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Select;
 
 //与数据库通信/传输用mapper对象
 @Mapper
 public interface UserMapper {
     @Insert("insert into user(name,account_id,token,gmt_create,gmt_modified) values (#{name},#{accountId},#{token},#{gmtCreate},#{gmtModified})")
     void insert(User user);
+
+    @Select("select * from user where token = #{token}")
+    User findByToken(@Param("token") String token);//@Select中的#{}内的参数，会将下面这个方法中的形参自动传进去（只有参数是-类对象-的时候会自动传，不是的时候，就如这行所写，需要加@Param的注解）
 }
