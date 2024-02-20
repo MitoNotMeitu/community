08d6f0f3a5fbc46999ea60c08135a22c314c0a54
MitoNotMeitu <haruhi20030225@gmail.com>
2024-02-20 14:24:06 +0800

    add flyway migration support

---
+++ src/main/resources/db/migration/V1__Create_user_table.sql
@@ -0,0 +1,11 @@
+create table user
+(
+    id           INTEGER auto_increment,
+    account_id   varchar(100),
+    name         varchar(50),
+    token        varchar(36),
+    gmt_create   bigint,
+    gmt_modified bigint,
+    constraint user_pk
+        primary key (id)
+);
